9a6d18cd589ec2b524438304cb329d508b4c158d
DanielSandrin <danielsandrin32@hotmail.com>
2021-09-10 12:01:06 -0300

    inclusão no repositório

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,123 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { api } from '../../services/api';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+  description: string;
+}
+
+type EpisodeProps = {
+  episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter();
+
+  const { play } = usePlayer()
+
+  return (
+    <div className={styles.episode}>
+
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
+        </Link>
+        <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
+        <button type="button" onClick={() => play(episode)}>
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episodes => {
+    return {
+      params: {
+        slug: episodes.id
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+/*
+  fallback = false => ele não retorna nada apenas retorna 404
+  para renderizar esticamente deveria ser definido nos paths qual os episódio.
+
+  fallback = true => realizar a busca pelo lado do client ou seja pelo browser
+
+*/
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { slug } = ctx.params;
+
+  const { data } = await api.get(`/episodes/${slug}`)
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url
+  };
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24 // carrega a cada 24 horas
+  }
+}
